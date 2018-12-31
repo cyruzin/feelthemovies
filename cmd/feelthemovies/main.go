ac09e83510c0c6d212ed7ac4e693c1d40062c4c0
Cyro Dubeux <xorycx@gmail.com>
2018-12-31 18:37:14 -0300

    Main cleanup

--- cmd/feelthemovies/main.go
+++ cmd/feelthemovies/main.go
@@ -24,47 +24,4 @@ func main() {
 
 	log.Println("Connection OK!")
 
-	// r := model.Recommendation{
-	// 	UserID:    1,
-	// 	Title:     "The Best Soccer Movies of All Time",
-	// 	Type:      0,
-	// 	Body:      "Only the best movies, no bad movies are allowed!",
-	// 	Poster:    "PlQeeRjZkl",
-	// 	Backdrop:  "QrTtMklOpz",
-	// 	Status:    0,
-	// 	CreatedAt: time.Now(),
-	// 	UpdatedAt: time.Now(),
-	// }
-
-	// rec, err := model.CreateRecommendation(&r, db)
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// g := make(map[int64][]int)
-	// k := make(map[int64][]int)
-
-	// g[rec.ID] = []int{1, 5, 8}
-	// k[rec.ID] = []int{12, 2, 4}
-
-	// _, err = helper.Attach(g, "genre_recommendation", db)
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// _, err = helper.Attach(k, "keyword_recommendation", db)
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// data, err := helper.ToJSONIndent(rec)
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// fmt.Println(data)
 }
