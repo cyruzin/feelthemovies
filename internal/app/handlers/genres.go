afa38622a79fe5252fc5a834eaeaefd387efe47d
Cyro Dubeux <xorycx@gmail.com>
2019-01-07 06:36:22 -0300

    Travis with log

--- internal/app/handlers/genres.go
+++ internal/app/handlers/genres.go
@@ -143,6 +143,8 @@ func deleteGenre(w http.ResponseWriter, r *http.Request) {
 
 	d, err := db.DeleteGenre(id)
 
+	data, _ := db.GetGenres()
+	log.Println(data)
 	log.Println(err)
 
 	if err != nil {
