0b3be7904e25c9e63137d0d717da5f83c72d4328
Cyro Dubeux <dev09@sptcode.com>
2019-01-08 17:49:30 -0300

    2 new tests

--- internal/app/handlers/recommendation_test.go
+++ internal/app/handlers/recommendation_test.go
@@ -6,6 +6,11 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
+
+	"github.com/cyruzin/feelthemovies/internal/app/model"
+	"github.com/cyruzin/feelthemovies/internal/pkg/helper"
+	validator "gopkg.in/go-playground/validator.v9"
 )
 
 func TestGetRecommendationsSuccess(t *testing.T) {
@@ -40,6 +45,103 @@ func TestGetRecommendationSuccess(t *testing.T) {
 
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("Status code differs. Expected %d.\n Got %d", http.StatusOK, status)
+	}
+}
+
+func TestCreateRecommendation(t *testing.T) {
+
+	newRec := &model.Recommendation{
+		UserID:    1,
+		Title:     "Aquaman",
+		Type:      1,
+		Body:      "The new Aquaman movie",
+		Poster:    "ahs9qounasas",
+		Backdrop:  "ajsopqwhasn",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	validate = validator.New()
+	err = validate.Struct(newRec)
+
+	if err != nil {
+		t.Errorf("CreateRecommendation - Validation - error: %s", err)
+	}
+
+	rec, err := db.CreateRecommendation(newRec)
+
+	if err != nil {
+		t.Errorf("CreateRecommendation error: %s", err)
+	}
+
+	keywords := make(map[int64][]int)
+	genres := make(map[int64][]int)
+
+	keywords[rec.ID] = []int{1, 2}
+	genres[rec.ID] = []int{3, 5}
+
+	_, err = helper.Attach(keywords, "keyword_recommendation", db.DB)
+
+	if err != nil {
+		t.Errorf("CreateRecommendation - Attach - error: %s", err)
+	}
+
+	_, err = helper.Attach(genres, "genre_recommendation", db.DB)
+
+	if err != nil {
+		t.Errorf("CreateRecommendation - Attach - error: %s", err)
+	}
+}
+
+func TestUpdateRecommendation(t *testing.T) {
+
+	// Check status
+	validate = validator.New()
+	err = validate.Var(1, "required,min=1,max=2")
+
+	if err != nil {
+		t.Errorf("UpdateRecommendation - Validation - error: %s", err)
+	}
+
+	upRec := &model.Recommendation{
+		Title:     "Aquaman",
+		Type:      1,
+		Body:      "The new Aquaman movie",
+		Status:    2,
+		Poster:    "ahs9qounasas",
+		Backdrop:  "ajsopqwhasn",
+		UpdatedAt: time.Now(),
+	}
+
+	validate = validator.New()
+	err = validate.Struct(upRec)
+
+	if err != nil {
+		t.Errorf("CreateRecommendation - Validation - error: %s", err)
+	}
+
+	rec, err := db.UpdateRecommendation(2, upRec)
+
+	if err != nil {
+		t.Errorf("UpdateRecommendation error: %s", err)
+	}
+
+	keywords := make(map[int64][]int)
+	genres := make(map[int64][]int)
+
+	keywords[rec.ID] = []int{1, 2}
+	genres[rec.ID] = []int{3, 5}
+
+	_, err = helper.Attach(keywords, "keyword_recommendation", db.DB)
+
+	if err != nil {
+		t.Errorf("UpdateRecommendation - Attach - error: %s", err)
+	}
+
+	_, err = helper.Attach(genres, "genre_recommendation", db.DB)
+
+	if err != nil {
+		t.Errorf("UpdateRecommendation - Attach - error: %s", err)
 	}
 }
 
