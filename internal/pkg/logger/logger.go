d638282d3dd89c0f0c2277d21e57db12c95be4c9
Cyro Dubeux <xorycx@gmail.com>
2019-07-01 00:53:41 -0300

    Added Logger EnvMode  check

--- internal/pkg/logger/logger.go
+++ internal/pkg/logger/logger.go
@@ -1,6 +1,8 @@
 package logger
 
 import (
+	"os"
+
 	"go.uber.org/zap"
 )
 
@@ -11,9 +13,15 @@ type Logger struct {
 
 // Init initiates the logger.
 func Init() (*Logger, error) {
-	log, err := zap.NewProduction() // Uber Zap Logger instance.
+	log, err := zap.NewDevelopment()
+
+	if env := os.Getenv("ENVMODE"); env == "production" {
+		log, err = zap.NewProduction()
+	}
+
 	if err != nil {
 		return nil, err
 	}
+
 	return &Logger{log.Sugar()}, nil
 }
